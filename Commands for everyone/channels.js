487480ab120a898028e48ed5a40ebedae3b28b36
NaveTwizer <86551796+NaveTwizer@users.noreply.github.com>
2021-07-08 13:16:03 +0300

    Update channels.js

--- Commands for everyone/channels.js
+++ Commands for everyone/channels.js
@@ -2,10 +2,10 @@ module.exports = {
     name: 'channels',
     description: 'Get info on all channels on the server',
     async execute(message, Discord) {
-        let textChannelCount = 0;
-        let voiceChannelCount = 0;
+        let textChannelCount = 0; // shows how much text channels are there
+        let voiceChannelCount = 0; // shows how much VC's are there
 
-        message.guild.channels.cache.forEach(channel => {
+        message.guild.channels.cache.forEach(channel => { // loop that goes for each channel on the server
             if (channel.type === 'text') {
                 textChannelCount++;
             }
@@ -13,7 +13,7 @@ module.exports = {
                 voiceChannelCount++;
             }
         });
-        let channelsEmbed = new Discord.MessageEmbed()
+        let channelsEmbed = new Discord.MessageEmbed() // set up embed message uwu
         .addFields(
             {name: 'Channels info for the server', value: `${message.guild.name}`},
             {name: 'Text channels', value: textChannelCount},
