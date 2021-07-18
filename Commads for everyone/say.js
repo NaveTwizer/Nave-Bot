f66ba23b9525243c1c7873f1b1effd99cec4c8db
NaveTwizer <86551796+NaveTwizer@users.noreply.github.com>
2021-07-18 14:25:54 +0300

    Update say.js

--- Commads for everyone/say.js
+++ Commads for everyone/say.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'say',
     description: 'Say any message using the bot',
+    category: 'test',
     async execute(message, args) {
 
         if (!args[0]) return message.reply('Add message content!');
@@ -8,7 +9,12 @@ module.exports = {
         for (let i = 0; i < args.length; i++) {
             sentence += args[i] + ' ';
         }
+        if (sentence.includes('@everyone')) return message.reply('Nice try buddy!');
+        // to make sure nobody can ping @everyone using the bot
         await message.channel.send(sentence);
-        message.delete();
+        if (message.guild.me.hasPermission('MANAGE_MESSAGES')) { // to make sure the bot can delete messages, if not keep the message
+            message.delete();
+        }
+        
     }
 }
